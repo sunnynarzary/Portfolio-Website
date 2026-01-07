5c248cf8c7a11defe6eef320452d3aa28562094a
sunnynarzary <townhalllevel7@gmail.com>
2026-01-07 13:59:18 +0530

    CI/CD pipelines

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  // GitHub Pages doesn't support Next Image optimization
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
